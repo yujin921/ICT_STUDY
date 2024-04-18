25ec27f43c93b749bdb92c33553725dd901408e1
user <user@DESKTOP-2AC9454>
2024-04-18 18:47:16 +0900

    d

---
+++ java45/src/Chapter06/Array/Array_example02.java
@@ -0,0 +1,35 @@
+package Chapter06.Array;
+
+import java.util.Scanner;
+
+public class Array_example02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int user;
+		int sum = 0;
+		
+		do {
+			System.out.print("배열 크기 : ");
+			user = scan.nextInt();
+		}while(user<2);{}
+		
+		int arr[] = new int[user];
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.printf("arr[%d] = ",i);
+			int num = scan.nextInt();
+			arr[i] = num;
+			sum += arr[i];
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(i==(arr.length)-1) {
+				System.out.print(arr[i]+"=");
+			}else {
+				System.out.print(arr[i]+"+");
+			}
+		}
+		System.out.println(sum);
+
+	}
+}
