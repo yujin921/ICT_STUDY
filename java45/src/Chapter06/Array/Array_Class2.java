50140f1ada306529607a3b200919046aa4bb6a1d
yujin921 <74189777+yujin921@users.noreply.github.com>
2024-04-22 17:58:42 +0900

    Add files via upload

---
+++ java45/src/Chapter06/Array/Array_Class2.java
@@ -0,0 +1,16 @@
+package Chapter06.Array;
+//다시 올리기
+public class Array_Class2 {
+	public static void main(String[] args) {
+		int[] score = new int[5];
+		score[0] = 88;
+		score[1] = 99;
+		score[2] = 70;
+		score[3] = 55;
+		score[4] = 100;
+		
+		for(int i=0; i<score.length; i++) {
+			System.out.println((i+1)+"번 학생의 성적 : "+ score[i]);
+		}
+	}
+}
---
+++ java45/src/Chapter06/Array/Array_example3.java
@@ -0,0 +1,39 @@
+package Chapter06.Array;
+//다시 올리기
+import java.util.Scanner;
+
+public class Array_example3 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int user;
+		int sum = 0;
+		
+		do {
+			System.out.print("배열 크기 : ");
+			user = scan.nextInt();
+			
+			if(user<2) {
+				System.out.println("에러 발생");
+			}
+		}while(user<2);{}
+		
+		int arr[] = new int[user];
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.printf("arr[%d] = ",i);
+			int num = scan.nextInt();
+			arr[i] = num;
+			sum += arr[i];
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(i==(arr.length)-1) {
+				System.out.print(arr[i]+"=");
+			}else {
+				System.out.print(arr[i]+"+");
+			}
+		}
+		System.out.println(sum);
+
+	}
+}
